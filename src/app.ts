1477a6a7ff43cecaaab6ca798f1d08375c9ff5d8
kmriazuddin <mohammadriazuddin3@gmail.com>
2024-09-01 19:39:49 +0600

    addded a cors origin and credentials

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,12 @@ const app: Application = express();
 
 app.use(express.json());
 
-// app.use(cors());
-// app.use(
-//   cors({ origin: 'https://room-booking-redux.vercel.app', credentials: true }),
-// );
-
+app.use(
+  cors({
+    origin: 'https://l2-assignment-5-server.vercel.app',
+    credentials: true,
+  }),
+);
 app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 
 app.use(express.text());
